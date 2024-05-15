1c1156b5b52fca613986ab35243e94617ae95f98
Kishh14 <salvikishan833@gmai.com>
2024-05-15 18:34:47 +0530

    Fixes

--- src/components/Instruments/Instruments.jsx
+++ src/components/Instruments/Instruments.jsx
@@ -111,14 +111,15 @@ const Instruments = () => {
           {/* Record Button */}
           {isInstruments && (
             <button
-              className="Btn"
+              className=""
               onClick={() => {
                 startRecording();
                 howToRecord().moveNext();
               }}
               id="record-button"
             >
-              <div className="sign">
+              Record
+              {/* <div className="sign">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="512"
@@ -129,21 +130,21 @@ const Instruments = () => {
                   <path d="M384,138a117.93,117.93,0,0,0-91.84,192H219.84A118,118,0,1,0,128,374H384a118,118,0,0,0,0-236ZM54,256a74,74,0,1,1,74,74A74.09,74.09,0,0,1,54,256Zm330,74a74,74,0,1,1,74-74A74.09,74.09,0,0,1,384,330Z" />
                 </svg>
               </div>
-              <div className="rsp-text text-white">Record</div>
+              <div className="rsp-text text-white"></div> */}
             </button>
           )}
 
           {/* Stop Button */}
           {isRecording && (
             <button
-              className="Btn"
+              className=""
               onClick={() => {
                 stopRecording();
                 howToRecord().moveNext();
               }}
               id="stop-recording-button"
             >
-              <div className="sign">
+              {/* <div className="sign">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -156,22 +157,23 @@ const Instruments = () => {
                   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />{" "}
                   <path d="M5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3z" />{" "}
                 </svg>
-              </div>
-              <div className="rsp-text">Stop</div>
+              </div> */}
+              <div className="">Stop</div>
             </button>
           )}
 
           {/* Play Button */}
           {recordedAudioBlob && (
             <button
-              className="Btn"
+              className=""
               onClick={() => {
                 playRecording();
                 howToRecord().moveNext();
               }}
               id="play-recording-button"
             >
-              <div className="sign">
+              Play
+              {/* <div className="sign">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -183,17 +185,18 @@ const Instruments = () => {
                   {" "}
                   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />{" "}
                 </svg>
-              </div>
-              <div className="rsp-text">Play</div>
+              </div> */}
+              <div className=""></div>
             </button>
           )}
 
           {/* Download Button */}
           {recordedAudioBlob && (
-            <div className="download-button" onClick={downloadRecording}>
-              <div className="button-wrapper">
-                <div className="download-text">Download</div>
-                <span className="icon">
+            <div className="" onClick={downloadRecording}>
+              Download
+              <div className="">
+                <div className="download-text"></div>
+                {/* <span className="icon">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     aria-hidden="true"
@@ -212,7 +215,7 @@ const Instruments = () => {
                       d="M12 15V3m0 12l-4-4m4 4l4-4M2 17l.621 2.485A2 2 0 0 0 4.561 21h14.878a2 2 0 0 0 1.94-1.515L22 17"
                     ></path>
                   </svg>
-                </span>
+                </span> */}
               </div>
             </div>
           )}
